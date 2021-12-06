400eed46ff59870126014b8e901ebdd9cb53c304
Lawrence Wu <lawrencejwu@gmail.com>
2021-12-06 00:28:30 -0500

    Updates

--- _next/static/lGJtQF16Lja_1YyjBne7y/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-5cdb0f93451f199418c7.js"],"/_error":["static/chunks/pages/_error-e1bcaa043b7e8bc8fc85.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
